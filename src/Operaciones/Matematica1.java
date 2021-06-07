423b60d35aff49cec5f15bbdbf1cad423b2242b9
steven flores <floressteven105@gmail.com>
2021-06-07 18:19:01 -0500

    V1 Clase Matematica

---
+++ src/Operaciones/Matematica1.java
@@ -0,0 +1,11 @@
+
+package Operaciones;
+
+public class Matematica1 {
+    int a;
+    int b; 
+    public void suma() {
+    int resultado = a+b;
+    System.out.println("El resultado es: " + resultado);
+    }
+}
---
+++ src/Operaciones/PruebaMatematica.java
@@ -0,0 +1,14 @@
+
+package Operaciones;
+
+public class PruebaMatematica{
+    
+    public static void main(String[] args) {
+        Matematica1 ob1;
+        ob1= new Matematica1();
+        ob1.a = 35;
+        ob1.b = 25;
+        ob1.suma();   
+    }
+
+}
